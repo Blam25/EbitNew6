cebfb8f79994f1c3488512772de6b950153b9f38
blam25 <adrian.martvall@gmail.com>
2023-05-01 00:19:48 +0200

    nice

--- Game.go
+++ Game.go
@@ -1,19 +1,92 @@
-package EbitNew6
+package main
 
 import (
+	"sync"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type Game struct{}
 
 func (g *Game) Update() error {
+	var wg *sync.WaitGroup = &sync.WaitGroup{}
+
+	for _, s := range Systems {
+		wg.Add(1)
+		go s(wg)
+	}
+	wg.Wait()
+	//go TestSys()
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
+	var wg *sync.WaitGroup = &sync.WaitGroup{}
+
+	for _, s := range DrawSystems {
+		wg.Add(1)
+		go s(screen, wg)
+	}
+	wg.Wait()
+
+	//go TestSys2(screen)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 600, 400
+	return 640, 480
+}
+
+var Systems []func(wg *sync.WaitGroup)
+var DrawSystems []func(screen *ebiten.Image, wg *sync.WaitGroup)
+
+func init() {
+	Systems = append(Systems, TestSys)
+	Systems = append(Systems, TestSys)
+	Systems = append(Systems, TestSys)
+	Systems = append(Systems, TestSys)
+	Systems = append(Systems, TestSys)
+
+	DrawSystems = append(DrawSystems, TestSys2)
+}
+
+func TestSys(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	hmm := 1
+	what := false
+
+	Comps.Position.IterateWrite(func(i int, s *Position) {
+		s.X = s.X + hmm
+		print(s.X)
+		if s.X > 500 {
+			what = true
+		}
+	})
+
+	if what {
+		Comps.Position.IterateWrite(func(i int, s *Position) {
+			print("hejsan")
+		})
+	}
+
+	Comps.Position.IterateWrite(func(i int, s *Position) {
+		if Comps.Image.GetRead(i) != nil {
+
+		}
+	})
+}
+
+func TestSys2(screen *ebiten.Image, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	Comps.Position.IterateRead(func(i int, s Position) {
+		if a := Comps.Image.GetRead(i); a != nil {
+			a.op.GeoM.Reset()
+			a.op.GeoM.Translate(float64(s.X), float64(s.Y))
+			screen.DrawImage(a.image, &a.op)
+		}
+	})
 }
--- Nice2.go
+++ Nice2.go
@@ -1,56 +1,138 @@
-package EbitNew6
+package main
 
 import (
+	"sync"
 	//E "Nice2"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	//"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 var identifier int
 
 type Position struct {
+	Entity
+	*mu
 	X int
 	Y int
 }
 
+type mu struct {
+	*sync.Mutex
+}
+
+func (s *mu) LockMu() {
+	s.Lock()
+}
+
+func (s *mu) UnlockMu() {
+	s.Unlock()
+}
+
+type Image struct {
+	Entity
+	*mu
+	image *ebiten.Image
+	op    ebiten.DrawImageOptions
+}
+
 type Entity struct {
-	Id int
+	id int
 }
 
-func NewComp[T any]() *Component[T] {
-	new := &Component[T]{theMap: make(map[int]*T)}
+func (s Entity) Getid() int {
+	return s.id
+}
+
+type validComp interface {
+	Getid() int
+	LockMu()
+	UnlockMu()
+}
+
+func NewComp[T validComp]() *Component[T] {
+	new := &Component[T]{index: make(map[int]int)}
 	return new
-
 }
 
-type Component[T any] struct {
-	theMap map[int]*T
+type Component[T validComp] struct {
+	index    map[int]int
+	theArray []*T
+}
+
+func (s *Component[T]) Add(object *T) {
+	s.index[(*object).Getid()] = len(s.theArray)
+	s.theArray = append(s.theArray, object)
+}
+
+func (s *Component[T]) Remove(object *T) {
+	(*object).LockMu()
+	//index of object to be removed
+	index := s.index[(*object).Getid()]
+	//delete id and index of said object from map
+	delete(s.index, (*object).Getid())
+	//set value of deleted index to the last object in array, thereby deleting it
+	s.theArray[index] = s.theArray[len(s.theArray)-1]
+	//get id of moved index
+	movedId := (*s.theArray[index]).Getid()
+	//set new index of moved object correctly in map
+	s.index[movedId] = index
+	//delete the last (now duplicated) object from the array
+	s.theArray = s.theArray[:len(s.theArray)-1]
+	(*object).UnlockMu()
+}
+
+func (s *Component[T]) IterateWrite(f func(int, *T)) {
+
+	println("locked")
+	for i, s := range s.theArray {
+
+		(*s).LockMu()
+		f(i, s)
+		(*s).UnlockMu()
+	}
+	println("Unlocked")
+}
+
+func (s *Component[T]) IterateRead(f func(int, T)) {
+
+	for i, s := range s.theArray {
+		z := *s
+		f(i, z)
+	}
+}
+
+func (s *Component[T]) GetWrite(id int, f func(*T)) {
+
+	println("locked")
+	index := s.index[id]
+	comp := s.theArray[index]
+	(*comp).LockMu()
+	f(comp)
+	(*comp).UnlockMu()
+	println("Unlocked")
+}
+
+func (s *Component[T]) GetRead(id int) *T {
+
+	z := *s.theArray[id]
+	y := &z
+	return y
 }
 
 /*
-func Add[T any](object T, objectArray map[int]T) {
-	//objectArray = append(objectArray, object)
-	objectArray[s.Id] = object
-}*/
+	func remove(s []int, i int) []int {
+		s[i] = s[len(s)-1]
+		return s[:len(s)-1]
+	}
+*/
 
-func NewEntity() *Entity {
-	new := &Entity{}
-	new.Id = identifier
+func NewEntity() Entity {
+	new := Entity{}
+	new.id = identifier
 	identifier++
 	return new
-}
-
-var PosMap map[int]*Position
-var PosArr []*Position
-
-func init() {
-	PosMap = make(map[int]*Position)
-
-	//Ent1 := NewEntity()
-	//Ent1.Add(&Position{}, PosMap)
-
 }
 
 func main() {
@@ -62,4 +144,34 @@ func main() {
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type Components struct {
+	Position *Component[Position]
+	Image    *Component[Image]
+}
+
+var Comps *Components = &Components{}
+
+func init() {
+
+	Comps.Position = NewComp[Position]()
+	Comps.Image = NewComp[Image]()
+
+	var err error
+	image1, _, err := ebitenutil.NewImageFromFile("gopher.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Ent1 := NewEntity()
+	Comps.Position.Add(&Position{Ent1, &mu{&sync.Mutex{}}, 200, 200})
+	Comps.Image.Add(&Image{Entity: Ent1, image: image1})
+
+	Ent2 := NewEntity()
+	Comps.Position.Add(&Position{Ent2, &mu{&sync.Mutex{}}, 100, 100})
+	Comps.Image.Add(&Image{Entity: Ent2, image: image1})
+
+	//Ent1.Add(&Position{}, PosMap)
+
 }
